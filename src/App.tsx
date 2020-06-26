177f236392694ee1c164d3e427e63f22ea2fae8e
Fazlul Kabir Shohag <linkvisionsoft@yahoo.com>
2020-06-26 17:53:34 +0600

    children route added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
 import { Route } from 'react-router-dom';
-import routes from './routes/AppRouting.Module';
+import routes from './AppRouting.Module';
 
 class App extends React.Component {
   render() {
-    const routeComponents = routes.map(({ path, component }, key) =>
-      <Route exact path={path} component={component} key={key} />);
+    const routeComponents = routes.map(({ path, exact , component }, key) =>
+      <Route exact={exact} path={path} component={component} key={key} />);
     return (
       <main>
         <switch>
--- src/AppRouting.Module.tsx
+++ src/AppRouting.Module.tsx
@@ -1,11 +1,13 @@
-import SignIn from '../example/components/SignIn/SignIn';
-import Box from '../example/components/Box/Box';
+import SignIn from './example/components/SignIn/SignIn';
+import Box from './example/components/Box/Box';
 const routes = [
     {
+        exact: true,
         path: '/',
         component: Box
     },
     {
+        exact: false,
         path: '/sing',
         component: SignIn
     }
