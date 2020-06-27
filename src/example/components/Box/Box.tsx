998bd50eec25c5c9d5009ba0459bc873fd4e53fd
Fazlul Kabir Shohag <linkvisionsoft@yahoo.com>
2020-06-28 04:33:01 +0600

    store implemented

--- src/example/components/Box/Box.tsx
+++ src/example/components/Box/Box.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import store from '../../../store';
+import CounterAction from '../../../store/actions/CounterAction';
 
 export interface Props {
   name: string
@@ -11,6 +13,7 @@ export interface State {
 
 class Box extends React.Component<Props, State> {
 
+  public value: number = 0;
   constructor(props:Props) {
     super(props)
     this.state = {
@@ -18,19 +21,24 @@ class Box extends React.Component<Props, State> {
     }
   }  
 
-
   username: String = 'sdf';
   public onShowValue = (val: number): void => {
     this.setState({value:val});
   }
 
+  private increment(): void {
+    store.dispatch(CounterAction.increment());
+  }
+
   componentWillMount() {
+    this.increment();
+    console.log(store.getState());
   }
   
 
   render() {
     return <div>
-      <h1>Hello World</h1>
+      <h1>{store.getState().counter.val}</h1>
     </div>
   }
 }
